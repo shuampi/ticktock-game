15bcb4d1d071783fd86a098b890e239f27f7cae9
shuamp <castel.davila@gmail.com>
2022-09-03 11:40:15 +0100

    board component updated

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,9 +1,25 @@
 
 import React from 'react'
+import Square from '../Square/Square'
 
-const Board = () => {
+interface boardProps {
+    square:string;
+    onClick:()=> "value";
+}
+
+const Board = ({square, onClick}: boardProps) => {
   return (
-    <div>Board</div>
+    <div>
+        <Square value="1" onClick={()=> onClick()}/>
+        <Square value="2" onClick={()=> onClick()}/>
+        <Square value="3" onClick={()=> onClick()}/>
+        <Square value="4" onClick={()=> onClick()}/>
+        <Square value="5" onClick={()=> onClick()}/>
+        <Square value="6" onClick={()=> onClick()}/>
+        <Square value="7" onClick={()=> onClick()}/>
+        <Square value="8" onClick={()=> onClick()}/>
+        <Square value="9" onClick={()=> onClick()}/>
+    </div>
   )
 }
 
