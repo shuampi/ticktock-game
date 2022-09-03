ad8da83ea2730d7cf2c43b08cfdecbd0a2ae0a2a
shuamp <castel.davila@gmail.com>
2022-09-03 16:12:42 +0100

    components have been scafolded

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -3,22 +3,23 @@ import React from 'react'
 import Square from '../Square/Square'
 
 interface boardProps {
-    square:string;
-    onClick:()=> "value";
+    square?:string;
+    onClick?:()=> string;
+    
 }
 
 const Board = ({square, onClick}: boardProps) => {
   return (
     <div>
-        <Square value="1" onClick={()=> onClick()}/>
-        <Square value="2" onClick={()=> onClick()}/>
-        <Square value="3" onClick={()=> onClick()}/>
-        <Square value="4" onClick={()=> onClick()}/>
-        <Square value="5" onClick={()=> onClick()}/>
-        <Square value="6" onClick={()=> onClick()}/>
-        <Square value="7" onClick={()=> onClick()}/>
-        <Square value="8" onClick={()=> onClick()}/>
-        <Square value="9" onClick={()=> onClick()}/>
+        <Square value="1" onClick={()=> onClick}/>
+        <Square value="2" onClick={()=> onClick}/>
+        <Square value="3" onClick={()=> onClick}/>
+        <Square value="4" onClick={()=> onClick}/>
+        <Square value="5" onClick={()=> onClick}/>
+        <Square value="6" onClick={()=> onClick}/>
+        <Square value="7" onClick={()=> onClick}/>
+        <Square value="8" onClick={()=> onClick}/>
+        <Square value="9" onClick={()=> onClick}/>
     </div>
   )
 }
--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Board from '../Board/Board'
+
+// interface gameProps{
+    
+// }
 
 const Game = () => {
+    const handleClick = () => {
+return "value"
+    }
+    // const jumpTo = () => {
+
+    // }
+    // const renderMoves = () => {
+
+    // }
+
   return (
-    <div>Game</div>
+    <div>
+        <Board onClick={handleClick}/>
+    </div>
+    
   )
 }
 
